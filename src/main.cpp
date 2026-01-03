42abb53686a2757dc0b46dbd77d7fa075232de4e
Hady <handmasterdev@gmail.com>
2026-01-03 19:22:48 +0800

    nvim bullshit

--- src/main.cpp
+++ src/main.cpp
@@ -1,149 +1,170 @@
-#include <core/renderer.h>
-#include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <iostream>
-#include <loader/loader.h>
-#include <player/player.h>
-#include <shader/shaderCompiler.h>
-#include <window/windowManager.h>
+#include<glad/glad.h>
+#include<loader/loader.h>
+#include<shader/shaderCompiler.h>
+#include<window/windowManager.h>
+#include<core/renderer.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<player/player.h>
+#include<iostream>
 
 using namespace std;
 
-void mouseCallBack(GLFWwindow *window, double xPosIn, double yPosIn);
+void mouseCallBack(GLFWwindow* window,double xPosIn,double yPosIn);
 
 bool mouseClick = true;
-float lastX = windowManager.getWindowSizeW() / 2;
-float lastY = windowManager.getWindowSizeH() / 2;
+float lastX = windowManager.getWindowSizeW()/2;
+float lastY = windowManager.getWindowSizeH()/2;
 
-int main() {
-  windowManager.windowInit();
-  glfwSetCursorPosCallback(windowManager.getWindowID(), mouseCallBack);
-  windowManager.setWindowSize(500, 250);
-  windowManager.setWindowTitle("penguin");
+int main(){
+    windowManager.windowInit();
+    glfwSetCursorPosCallback(windowManager.getWindowID(),mouseCallBack);
+    windowManager.setWindowSize(500,250);
+    windowManager.setWindowTitle("penguin");
 
-  ShaderCompiler shader("/home/handdev/penguin/shader/shader.vert",
-                        "/home/handdev/penguin/shader/shader.frag");
+    ShaderCompiler shader("/home/handdev/penguin/shader/shader.vert",
+        "/home/handdev/penguin/shader/shader.frag");
 
-  //~~~~~~~~learnOpengl.com~~~~~~~~~~
-  vector<float> vertices = {
-      0.5f,  0.5f,  0.5f,  0,  0,  1,  0.5f,  -0.5f, 0.5f,  0,  0,  1,
-      -0.5f, -0.5f, 0.5f,  0,  0,  1,  -0.5f, 0.5f,  0.5f,  0,  0,  1,
+    //~~~~~~~~learnOpengl.com~~~~~~~~~~
+    vector<float> vertices = {
+         0.5f, 0.5f, 0.5f,  0,0,1,
+         0.5f,-0.5f, 0.5f,  0,0,1,
+        -0.5f,-0.5f, 0.5f,  0,0,1,
+        -0.5f, 0.5f, 0.5f,  0,0,1,
 
-      0.5f,  0.5f,  -0.5f, 0,  0,  -1, -0.5f, 0.5f,  -0.5f, 0,  0,  -1,
-      -0.5f, -0.5f, -0.5f, 0,  0,  -1, 0.5f,  -0.5f, -0.5f, 0,  0,  -1,
+         0.5f, 0.5f,-0.5f,  0,0,-1,
+        -0.5f, 0.5f,-0.5f,  0,0,-1,
+        -0.5f,-0.5f,-0.5f,  0,0,-1,
+         0.5f,-0.5f,-0.5f,  0,0,-1,
 
-      0.5f,  -0.5f, 0.5f,  -1, 0,  0,  0.5f,  -0.5f, -0.5f, -1, 0,  0,
-      0.5f,  0.5f,  -0.5f, -1, 0,  0,  0.5f,  0.5f,  0.5f,  -1, 0,  0,
+         0.5f,-0.5f, 0.5f,  -1,0,0,
+         0.5f,-0.5f,-0.5f,  -1,0,0,
+         0.5f, 0.5f,-0.5f,  -1,0,0,
+         0.5f, 0.5f, 0.5f,  -1,0,0,
 
-      -0.5f, -0.5f, -0.5f, 1,  0,  0,  -0.5f, 0.5f,  -0.5f, 1,  0,  0,
-      -0.5f, 0.5f,  0.5f,  1,  0,  0,  -0.5f, -0.5f, 0.5f,  1,  0,  0,
+         -0.5f,-0.5f,-0.5f,  1,0,0,
+         -0.5f, 0.5f,-0.5f,  1,0,0,
+         -0.5f, 0.5f, 0.5f,  1,0,0,
+         -0.5f,-0.5f, 0.5f,  1,0,0,
 
-      0.5f,  0.5f,  0.5f,  0,  1,  0,  -0.5f, 0.5f,  0.5f,  0,  1,  0,
-      -0.5f, 0.5f,  -0.5f, 0,  1,  0,  0.5f,  0.5f,  -0.5f, 0,  1,  0,
+          0.5f, 0.5f, 0.5f,  0,1,0,
+         -0.5f, 0.5f, 0.5f,  0,1,0,
+         -0.5f, 0.5f,-0.5f,  0,1,0,
+          0.5f, 0.5f,-0.5f,  0,1,0,
 
-      0.5f,  -0.5f, 0.5f,  0,  -1, 0,  -0.5f, -0.5f, 0.5f,  0,  -1, 0,
-      -0.5f, -0.5f, -0.5f, 0,  -1, 0,  0.5f,  -0.5f, -0.5f, 0,  -1, 0};
+          0.5f,-0.5f, 0.5f,  0,-1,0,
+         -0.5f,-0.5f, 0.5f,  0,-1,0,
+         -0.5f,-0.5f,-0.5f,  0,-1,0,
+          0.5f,-0.5f,-0.5f,  0,-1,0
+    };
 
-  vector<unsigned int> indices = {0,  1,  2,  2,  3,  0,
 
-                                  4,  5,  6,  6,  7,  4,
+    vector<unsigned int> indices = {
+        0, 1, 2,
+        2, 3, 0,
 
-                                  8,  9,  10, 10, 11, 8,
+        4, 5, 6,
+        6, 7, 4,
 
-                                  12, 13, 14, 14, 15, 12,
+        8, 9, 10,
+        10, 11, 8,
 
-                                  16, 17, 18, 18, 19, 16,
+        12, 13, 14,
+        14, 15, 12, 
 
-                                  20, 21, 22, 22, 23, 20};
-  //~
+        16, 17, 18,
+        18, 19, 16,
 
-  Loader quad(vertices, indices,
-              "/home/handdev/penguin/res/image/test_tex.png");
-  Renderer renQuad(quad.loadToVao(), quad.texture());
-  Loader quad2(vertices, indices, "/home/handdev/penguin/res/image/wall.jpg");
-  Renderer renQuad2(quad2.loadToVao(), quad2.texture());
-  player.setup(10, 2, 7);
-  player.setPosPlayer(glm::vec3(0, 0, 0));
-  // model mat4
-  glm::mat4 model;
-  float i = 0.0f;
-  //~
-  float deltaTime = 0;
-  float lastFrame = 0;
-
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-
-  while (!glfwWindowShouldClose(windowManager.getWindowID())) {
-    float currentFrame = static_cast<float>(glfwGetTime());
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-    windowManager.setColor(0, 0, 0);
-    player.loop(deltaTime);
-    shader.use();
-    shader.setVec3("lightPos", 1.2f, 1.0f, 2.0f);
-    shader.setVec3("viewPos", player.getPosCamera());
-    player.addShader(shader);
-
-    glm::vec3 lightColor = glm::vec3(1, 1, 1);
-    // lightColor.x = static_cast<float>(sin(glfwGetTime()*2));
-    // lightColor.y = static_cast<float>(sin(glfwGetTime()*0.7f));
-    // lightColor.z = static_cast<float>(sin(glfwGetTime()*1.3f));
-    glm::vec3 diffColor = lightColor * glm::vec3(0.5f);
-    glm::vec3 ambientColor = diffColor * glm::vec3(0.2f);
-    shader.setVec3("light.ambient", ambientColor);
-    shader.setVec3("light.diffuse", diffColor);
-    shader.setVec3("light.specular", 1, 1, 1);
-
-    shader.setVec3("material.ambient", 1, 0.5f, 0.31f);
-    shader.setVec3("material.diffuse", 1, 0.5f, 0.31f);
-    shader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-    shader.setFloat("material.shininess", 32);
-
-    // maths
-    model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0, 1, 0));
-    model = glm::rotate(model, glm::radians(i), glm::vec3(1, 1, 1));
-    i++;
-    shader.setMat4("model", model);
-    renQuad.render(shader, 36);
+        20, 21, 22,
+        22, 23, 20
+    };
     //~
 
-    model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0, 0, 0));
-    model = glm::scale(model, glm::vec3(20, 1, 20));
-    shader.setMat4("model", model);
-    renQuad2.render(shader, 36);
+    Loader quad(vertices,indices,"/home/handdev/penguin/res/image/test_tex.png");
+    Renderer renQuad(quad.loadToVao(),quad.texture());
+    Loader quad2(vertices,indices,"/home/handdev/penguin/res/image/wall.jpg");
+    Renderer renQuad2(quad2.loadToVao(),quad2.texture());
+    player.setup(10,2,7);
+    player.setPosPlayer(glm::vec3(0,0,0));
+    //model mat4
+    glm::mat4 model;
+    float i = 0.0f;
+    //~ 
+    float deltaTime = 0;
+    float lastFrame = 0;
 
-    windowManager.prepare();
-  }
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-  // cleanUp
-  shader.cleanUp();
-  quad.cleanUp();
-  quad2.cleanUp();
-  player.cleanUp();
-  windowManager.cleanUp();
-  // delete
-  return 0;
+    while(!glfwWindowShouldClose(windowManager.getWindowID())){
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        windowManager.setColor(0,0,0);
+        player.loop(deltaTime);
+        shader.use();
+        shader.setVec3("lightPos", 1.2f, 1.0f, 2.0f);
+        shader.setVec3("viewPos",player.getPosCamera());
+        player.addShader(shader);
+
+        glm::vec3 lightColor = glm::vec3(1,1,1);
+        // lightColor.x = static_cast<float>(sin(glfwGetTime()*2));
+        // lightColor.y = static_cast<float>(sin(glfwGetTime()*0.7f));
+        // lightColor.z = static_cast<float>(sin(glfwGetTime()*1.3f));
+        glm::vec3 diffColor = lightColor * glm::vec3(0.5f);
+        glm::vec3 ambientColor = diffColor * glm::vec3(0.2f);
+        shader.setVec3("light.ambient",ambientColor);
+        shader.setVec3("light.diffuse",diffColor);
+        shader.setVec3("light.specular",1,1,1);
+
+        shader.setVec3("material.ambient",1,0.5f,0.31f);
+        shader.setVec3("material.diffuse",1,0.5f,0.31f);
+        shader.setVec3("material.specular",0.5f,0.5f,0.5f);
+        shader.setFloat("material.shininess",32);
+        
+        //maths
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0,1,0));
+        model = glm::rotate(model,glm::radians(i),glm::vec3(1,1,1));
+        i++;
+        shader.setMat4("model", model);
+        renQuad.render(shader,36);
+        //~
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model,glm::vec3(0,0,0));
+        model = glm::scale(model,glm::vec3(20,1,20));
+        shader.setMat4("model",model);
+        renQuad2.render(shader,36);
+
+        windowManager.prepare();
+    }
+
+    //cleanUp
+    shader.cleanUp();
+    quad.cleanUp();
+    quad2.cleanUp();
+    player.cleanUp();
+    windowManager.cleanUp();
+    //delete
+    return 0;
 }
 
-void mouseCallBack(GLFWwindow *window, double xPosIn, double yPosIn) {
-  float xPos = static_cast<float>(xPosIn);
-  float yPos = static_cast<float>(yPosIn);
+void mouseCallBack(GLFWwindow* window,double xPosIn,double yPosIn){
+    float xPos = static_cast<float>(xPosIn);
+    float yPos = static_cast<float>(yPosIn);
 
-  if (mouseClick) {
+    if(mouseClick){
+        lastX = xPos;
+        lastY = yPos;
+        mouseClick = false;
+    }
+
+    float xOffset = xPos - lastX;
+    float yOffset = lastY - yPos;
     lastX = xPos;
     lastY = yPos;
-    mouseClick = false;
-  }
-
-  float xOffset = xPos - lastX;
-  float yOffset = lastY - yPos;
-  lastX = xPos;
-  lastY = yPos;
-  if (!windowManager.getIsPause()) {
-    player.forwardMovementMouse(xOffset, yOffset);
-  }
+    if(!windowManager.getIsPause()){
+        player.forwardMovementMouse(xOffset,yOffset);
+    }
 }
--- src/window/windowManager.cpp
+++ src/window/windowManager.cpp
@@ -1,99 +1,109 @@
-#include <window/windowManager.h>
 
-void frameBufferSizeCallBack(GLFWwindow *window, int w, int h);
+#include<window/windowManager.h>
 
-void WindowManager::windowInit() {
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+void frameBufferSizeCallBack(GLFWwindow* window,int w,int h);
 
-  window = glfwCreateWindow(w, h, title, NULL, NULL);
+void WindowManager::windowInit(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 
-  if (window == 0) {
-    cout << "ERROR::window is null!" << endl;
+    window = glfwCreateWindow(w,h,title,NULL,NULL);
+
+    if(window==0){
+        cout<<"ERROR::window is null!"<<endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window,frameBufferSizeCallBack);
+    // glfwSetCursorPosCallback(window,mouseCallBack);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+    }
+    glEnable(GL_DEPTH_TEST);
+}
+
+void WindowManager::prepare(){
+    statePause();
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+void WindowManager::cleanUp(){
+    glfwDestroyWindow(window);
     glfwTerminate();
-  }
-  glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window, frameBufferSizeCallBack);
-  // glfwSetCursorPosCallback(window,mouseCallBack);
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << std::endl;
-    glfwTerminate();
-  }
-  glEnable(GL_DEPTH_TEST);
 }
 
-void WindowManager::prepare() {
-  statePause();
-
-  glfwSwapBuffers(window);
-  glfwPollEvents();
+void WindowManager::setWindowTitle(const char* _title){
+    glfwSetWindowTitle(window,_title);
+    title = _title;
 }
 
-void WindowManager::cleanUp() {
-  glfwDestroyWindow(window);
-  glfwTerminate();
+void WindowManager::setWindowSize(int w,int h){
+    glfwSetWindowSize(window,w,h);
 }
 
-void WindowManager::setWindowTitle(const char *_title) {
-  glfwSetWindowTitle(window, _title);
-  title = _title;
+void WindowManager::setColor(int x,int y,int z){
+    glClearColor(x,y,z,1);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-void WindowManager::setWindowSize(int w, int h) {
-  glfwSetWindowSize(window, w, h);
+void WindowManager::getWindowSizeUpdate(int &w,int &h){
+    this->w = w;
+    this->h = h;
 }
 
-void WindowManager::setColor(int x, int y, int z) {
-  glClearColor(x, y, z, 1);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+GLFWwindow* WindowManager::getWindowID()const{
+    return window;
 }
 
-void WindowManager::getWindowSizeUpdate(int &w, int &h) {
-  this->w = w;
-  this->h = h;
+int WindowManager::getWindowSizeH()const{
+    return h;
 }
 
-GLFWwindow *WindowManager::getWindowID() const { return window; }
-
-int WindowManager::getWindowSizeH() const { return h; }
-
-int WindowManager::getWindowSizeW() const { return w; }
-
-bool WindowManager::getIsPause() const { return isPause; }
-
-const char *WindowManager::getTitleString() const { return title; }
-
-// private
-void WindowManager::statePause() {
-  // enter pause mode!
-  if (glfwGetKey(windowManager.getWindowID(), GLFW_KEY_ESCAPE)) {
-    isPause = true;
-    glfwSetCursorPos(this->window, w / 2, h / 2);
-  }
-
-  //~
-
-  // aut of pause mode!
-  if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)) {
-    isPause = false;
-  }
-  // ~
-
-  if (isPause) {
-    glfwSetInputMode(windowManager.getWindowID(), GLFW_CURSOR,
-                     GLFW_CURSOR_NORMAL);
-  } else {
-    glfwSetInputMode(windowManager.getWindowID(), GLFW_CURSOR,
-                     GLFW_CURSOR_DISABLED);
-  }
+int WindowManager::getWindowSizeW()const{
+    return w;
 }
 
-void frameBufferSizeCallBack(GLFWwindow *window, int wV, int hV) {
-  glViewport(0, 0, wV, hV);
-  windowManager.getWindowSizeUpdate(wV, hV);
+bool WindowManager::getIsPause()const{
+    return isPause;
+}
+
+const char* WindowManager::getTitleString()const{
+    return title;
+}
+
+//private
+void WindowManager::statePause(){
+    //enter pause mode!
+    if(glfwGetKey(windowManager.getWindowID(), GLFW_KEY_ESCAPE)){
+        isPause = true;
+        glfwSetCursorPos(this->window, w/2, h/2);
+    }
+
+    //~
+
+    //aut of pause mode!
+    if(glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)){
+        isPause = false;
+    }
+    // ~
+
+  if(isPause){
+        glfwSetInputMode(windowManager.getWindowID(),GLFW_CURSOR,GLFW_CURSOR_NORMAL);
+    }else{
+        glfwSetInputMode(windowManager.getWindowID(),GLFW_CURSOR,GLFW_CURSOR_DISABLED);
+    }
+}
+
+void frameBufferSizeCallBack(GLFWwindow* window,int wV,int hV){
+    glViewport(0,0,wV,hV);
+    windowManager.getWindowSizeUpdate(wV,hV);
 }
 WindowManager windowManager;
